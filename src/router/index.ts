533e293214bd1820a9bb6c5268775b6db2af20e2
Diego Reyes <diegorele13@gmail.com>
2025-12-22 11:00:34 -0500

    feat(router): add events and event detail routes
    Add new routes for events listing and event detail pages to enable navigation to these views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/home/index.vue'
 import MenuPage from '@/views/menu/index.vue'
+import EventsPage from '@/views/events/index.vue'
+import EventDetail from '@/views/events/EventDetail.vue'
 import ReservationsPage from '@/views/reservations/index.vue'
 import NotFoundPage from '@/views/NotFound.vue'
 
@@ -9,6 +11,8 @@ const router = createRouter({
   routes: [
     { path: '/', name: 'home', component: HomePage },
     { path: '/menu', name: 'menu', component: MenuPage },
+    { path: '/events', name: 'events', component: EventsPage },
+    { path: '/events/:slug', name: 'event-detail', component: EventDetail },
     { path: '/reservations', name: 'reservations', component: ReservationsPage },
     { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFoundPage },
   ],
